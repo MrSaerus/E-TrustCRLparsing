8b1ecd98b99449fdbeddea25a21c19ac36c184a0
Saerus <saerus.sama@gmail.com>
2021-03-05 14:18:38 +0500

    Updated the function addable custоm CRL

--- main.py
+++ main.py
@@ -1037,7 +1037,7 @@ class MainWindow(QMainWindow):
                 button_crl_save.setFixedSize(100, 30)
                 button_crl_save.setText("Скачать")
                 button_crl_save.pressed.connect(
-                    lambda u=row.UrlCRL, s=row.Stamp: download_file(u, s + '.crl', config['Folders']['crls']))
+                    lambda u=row.UrlCRL, s=row.KeyId: download_file(u, s + '.crl', config['Folders']['crls']))
                 self.ui.tableWidget_3.setCellWidget(count, 6, button_crl_save)
 
                 button_add_to_watch = QPushButton()
@@ -1971,63 +1971,128 @@ class UcWindow(QWidget):
 
 class AddCRLWindow(QWidget):
     def __init__(self):
-        super().__init__()
-        self.ui_add = Ui_Form_add()
-        self.ui_add.setupUi(self)
-        self.setWindowIcon(QIcon('assists/favicon.ico'))
-        self.ui_add.lineEdit.textChanged[str].connect(self.init)
-        self.ui_add.pushButton.pressed.connect(self.set_fields)
-        self.ui_add.pushButton_2.pressed.connect(self.query_fields)
-        self.init()
+        try:
+            super().__init__()
+            self.ui_add = Ui_Form_add()
+            self.ui_add.setupUi(self)
+            self.setWindowIcon(QIcon('assists/favicon.ico'))
+            self.ui_add.lineEdit.textChanged[str].connect(self.init)
+            self.ui_add.pushButton.pressed.connect(self.set_fields)
+            self.ui_add.pushButton_2.pressed.connect(self.query_fields)
+            self.init()
+        except Exception:
+            print('Error: AddCRLWindow()::__init__()', 'errors')
+            logs('Error: AddCRLWindow()::__init__()', 'errors')
 
     def init(self, text=''):
-        self.ui_add.comboBox.clear()
-        query = CERT.select().where(CERT.Registration_Number.contains(text)
-                                    | CERT.Name.contains(text)
-                                    | CERT.KeyId.contains(text)
-                                    | CERT.Stamp.contains(text)
-                                    | CERT.SerialNumber.contains(text)).limit(config['Listing']['cert'])
-        for row in query:
-            self.ui_add.comboBox.addItem(row.Name, row.ID)
+        try:
+            self.ui_add.comboBox.clear()
+            query = CERT.select().where(CERT.Registration_Number.contains(text)
+                                        | CERT.Name.contains(text)
+                                        | CERT.KeyId.contains(text)
+                                        | CERT.Stamp.contains(text)
+                                        | CERT.SerialNumber.contains(text)).limit(config['Listing']['cert'])
+            for row in query:
+                self.ui_add.comboBox.addItem(row.Name, row.KeyId)
+        except Exception:
+            print('Error: AddCRLWindow()::init()', 'errors')
+            logs('Error: AddCRLWindow()::init()', 'errors')
 
     def set_fields(self):
-        id_cert = self.ui_add.comboBox.currentIndex()
-        query = CERT.select().where(CERT.ID == id_cert)
-        registration_number = 0
-        for row_cert in query:
-            registration_number = row_cert.Registration_Number
-            self.ui_add.lineEdit_6.setText(str(row_cert.Name))
-            self.ui_add.lineEdit_3.setText(str(row_cert.KeyId))
-            self.ui_add.lineEdit_8.setText(str(row_cert.Stamp))
-            self.ui_add.lineEdit_4.setText(str(row_cert.SerialNumber))
-        query_2 = UC.select().where(UC.Registration_Number == registration_number)
-        for row_uc in query_2:
-            self.ui_add.lineEdit_7.setText(str(row_uc.INN))
-            self.ui_add.lineEdit_2.setText(str(row_uc.OGRN))
+        try:
+            id_cert = self.ui_add.comboBox.currentData()
+            query = CERT.select().where(CERT.KeyId == id_cert)
+            registration_number = 0
+            for row_cert in query:
+                registration_number = row_cert.Registration_Number
+                self.ui_add.lineEdit_6.setText(str(row_cert.Name))
+                self.ui_add.lineEdit_3.setText(str(row_cert.KeyId))
+                self.ui_add.lineEdit_8.setText(str(row_cert.Stamp))
+                self.ui_add.lineEdit_4.setText(str(row_cert.SerialNumber))
+                self.ui_add.lineEdit_5.setText(str(row_cert.Registration_Number))
+            query_2 = UC.select().where(UC.Registration_Number == registration_number)
+            for row_uc in query_2:
+                self.ui_add.lineEdit_7.setText(str(row_uc.INN))
+                self.ui_add.lineEdit_2.setText(str(row_uc.OGRN))
+        except Exception:
+            print('Error: AddCRLWindow()::set_fields()', 'errors')
+            logs('Error: AddCRLWindow()::set_fields()', 'errors')
 
     def query_fields(self):
-        if WatchingCRL.select().where(WatchingCRL.UrlCRL == self.ui_add.lineEdit_9.text()).count() > 0:
-            name = self.ui_add.lineEdit_6.text()
-            inn = self.ui_add.lineEdit_7.text()
-            ogrn = self.ui_add.lineEdit_2.text()
-            key_id = self.ui_add.lineEdit_3.text()
-            stamp = self.ui_add.lineEdit_8.text()
-            serial_number = self.ui_add.lineEdit_4.text()
-            url_crl = self.ui_add.lineEdit_9.text()
-
-            query = WatchingCustomCRL(Name=name,
-                                      INN=inn,
-                                      OGRN=ogrn,
-                                      KeyId=key_id,
-                                      Stamp=stamp,
-                                      SerialNumber=serial_number,
-                                      UrlCRL=url_crl,
-                                      status='Unknown',
-                                      download_status='Unknown',
-                                      download_count='0',
-                                      last_download='1970-01-01 00:00:00',
-                                      last_update='1970-01-01 00:00:00',
-                                      next_update='1970-01-01 00:00:00')
+        try:
+            if WatchingCRL.select().where(WatchingCRL.Name == self.ui_add.lineEdit_6.text()
+                                          or WatchingCRL.INN == self.ui_add.lineEdit_7.text()
+                                          or WatchingCRL.OGRN == self.ui_add.lineEdit_2.text()
+                                          or WatchingCRL.KeyId == self.ui_add.lineEdit_3.text()
+                                          or WatchingCRL.Stamp == self.ui_add.lineEdit_8.text()
+                                          or WatchingCRL.SerialNumber == self.ui_add.lineEdit_4.text()
+                                          or WatchingCRL.UrlCRL == self.ui_add.lineEdit_9.text()).count() > 0:
+                print('Info: CRL is exists in WatchingCRL')
+                logs('Info: CRL is exists in WatchingCRL')
+                self.ui_add.label_10.setText('CRL уже есть в основном списке отслеживания')
+            elif WatchingCustomCRL.select().where(WatchingCustomCRL.Name == self.ui_add.lineEdit_6.text()
+                                                  or WatchingCustomCRL.INN == self.ui_add.lineEdit_7.text()
+                                                  or WatchingCustomCRL.OGRN == self.ui_add.lineEdit_2.text()
+                                                  or WatchingCustomCRL.KeyId == self.ui_add.lineEdit_3.text()
+                                                  or WatchingCustomCRL.Stamp == self.ui_add.lineEdit_8.text()
+                                                  or WatchingCustomCRL.SerialNumber == self.ui_add.lineEdit_4.text()
+                                                  or WatchingCustomCRL.UrlCRL == self.ui_add.lineEdit_9.text())\
+                    .count() > 0:
+                print('Info: CRL is exist in WatchingCustomCRL')
+                logs('Info: CRL is exist in WatchingCustomCRL')
+                self.ui_add.label_10.setText('CRL уже есть в своем списке отслеживания')
+            elif WatchingDeletedCRL.select().where(WatchingDeletedCRL.Name == self.ui_add.lineEdit_6.text()
+                                                   or WatchingDeletedCRL.INN == self.ui_add.lineEdit_7.text()
+                                                   or WatchingDeletedCRL.OGRN == self.ui_add.lineEdit_2.text()
+                                                   or WatchingDeletedCRL.KeyId == self.ui_add.lineEdit_3.text()
+                                                   or WatchingDeletedCRL.Stamp == self.ui_add.lineEdit_8.text()
+                                                   or WatchingDeletedCRL.SerialNumber == self.ui_add.lineEdit_4.text()
+                                                   or WatchingDeletedCRL.UrlCRL == self.ui_add.lineEdit_9.text())\
+                    .count() > 0:
+                print('Info: CRL is exist in WatchingDeletedCRL')
+                logs('Info: CRL is exist in WatchingDeletedCRL')
+                self.ui_add.label_10.setText('CRL уже есть в удаленных, или удалите полностью или верните обратно')
+            else:
+                name = self.ui_add.lineEdit_6.text()
+                inn = self.ui_add.lineEdit_7.text()
+                ogrn = self.ui_add.lineEdit_2.text()
+                key_id = self.ui_add.lineEdit_3.text()
+                stamp = self.ui_add.lineEdit_8.text()
+                serial_number = self.ui_add.lineEdit_4.text()
+                url_crl = self.ui_add.lineEdit_9.text()
+                if name == '' or inn == '' or ogrn == '' or key_id == '' or stamp == '' or serial_number == '' or url_crl == '':
+                    print('Заполните все поля')
+                    print('Info: The fields should not be empty')
+                    logs('Info: The fields should not be empty')
+                    self.ui_add.label_10.setText('Заполните все поля')
+                else:
+                    query = WatchingCustomCRL(Name=name,
+                                              INN=inn,
+                                              OGRN=ogrn,
+                                              KeyId=key_id,
+                                              Stamp=stamp,
+                                              SerialNumber=serial_number,
+                                              UrlCRL=url_crl,
+                                              status='Unknown',
+                                              download_status='Unknown',
+                                              download_count='0',
+                                              last_download='1970-01-01 00:00:00',
+                                              last_update='1970-01-01 00:00:00',
+                                              next_update='1970-01-01 00:00:00')
+                    query.save()
+                    download_file(url_crl,
+                                  key_id + '.crl',
+                                  config['Folders']['crls'],
+                                  'custome',
+                                  str(query.ID),
+                                  set_dd='Yes')
+                    check_custom_crl(query.ID, name, key_id)
+                    print('Info: CRL added in WatchingCustomCRL')
+                    logs('Info: CRL added in WatchingCustomCRL')
+                    self.ui_add.label_10.setText('CRL "' + name + '" добавлен в список отслеживания')
+        except Exception:
+            print('Error: AddCRLWindow()::query_fields()', 'errors')
+            logs('Error: AddCRLWindow()::query_fields()', 'errors')
 
 
 if __name__ == "__main__":
